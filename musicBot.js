a7b396f585cf963cc1cd7df0ee896c4cf2ce8735
dany98sa <56975152+dany98sa@users.noreply.github.com>
2021-01-29 19:13:06 +0100

    Update musicBot.js

--- musicBot.js
+++ musicBot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
-const ytdl = require('ytdl-core');
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -13,21 +12,7 @@ client.login(process.env.tokenBotDiscord);
 
 client.on('message', message => {
 	if (message.content===p+'play') {
-		message.member.voice.channel.join().then(connection=>{
-			const stream = ytdl('',{filter:'audioonly'});
-			const dispatcher = connection.play(stream);
-			dispatcher.on('finish', () => message.member.voice.channel.leave());		
-		});
-		return;
-	}
-	var args=message.content.split(' ');
-	if(args[0]===p+'play'){
-		message.member.voice.channel.join().then(connection=>{
-			const stream = ytdl(args[1],{filter:'audioonly'});
-			const dispatcher = connection.play(stream);
-			dispatcher.on('finish', () => message.member.voice.channel.leave());
-		});
-		return;
+		message.author.send('play');
 	}
 });
 
