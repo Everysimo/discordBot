9740073c9b9e91f445f2e77c360e130100dc8266
dany98sa <56975152+dany98sa@users.noreply.github.com>
2021-01-29 19:20:47 +0100

    Update musicBot.js

--- musicBot.js
+++ musicBot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
+const ytdl = require('ytdl-core');
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -12,7 +13,21 @@ client.login(process.env.tokenBotDiscord);
 
 client.on('message', message => {
 	if (message.content===p+'play') {
-		message.author.send('play');
+		message.member.voice.channel.join().then(connection=>{
+			const stream = ytdl('https://www.youtube.com/watch?v=3ekFx8OXxtM',{filter:'audioonly'});
+			const dispatcher = connection.play(stream);
+			dispatcher.on('finish', () => message.member.voice.channel.leave());		
+		});
+		message.channel.send('https://www.youtube.com/watch?v=3ekFx8OXxtM');
+	}
+	var args=message.content.split(' ');
+	if(args[0]===p+'play'){
+		message.member.voice.channel.join().then(connection=>{
+			const stream = ytdl(args[1],{filter:'audioonly'});
+			const dispatcher = connection.play(stream);
+			dispatcher.on('finish', () => message.member.voice.channel.leave());
+		});
+		message.channel.send(args[1]);
 	}
 });
 
