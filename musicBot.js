07e7f6654d9aa4cbc37ce1d183c1278e6a160fcb
dany98sa <56975152+dany98sa@users.noreply.github.com>
2021-01-30 15:45:54 +0100

    codice aggiornato, aggiunte verifiche se l'utente è in una stanza vocale

--- musicBot.js
+++ musicBot.js
@@ -8,26 +8,33 @@ client.once('ready', () => {
 });
 
 var p=config.prefisso;
-
+var ry=config.reactionYes;
+var rn=config.reactionNot;
 client.login(process.env.tokenBotDiscord);
 
 client.on('message', message => {
 	if (message.content===p+'play') {
+		if (message.member.voice.channel==null) {
+			message.react(rn);
+		}
 		message.member.voice.channel.join().then(connection=>{
 			const stream = ytdl('https://www.youtube.com/watch?v=3ekFx8OXxtM',{filter:'videoandaudio'});
 			const dispatcher = connection.play(stream);
 			dispatcher.on('finish', () => message.member.voice.channel.leave());		
 		});
-		message.channel.send('https://www.youtube.com/watch?v=3ekFx8OXxtM');
+		message.react(ry);
 	}
 	var args=message.content.split(' ');
 	if(args[0]===p+'play'){
+		if (message.member.voice.channel==null) {
+			message.react(rn);
+		}
 		message.member.voice.channel.join().then(connection=>{
 			const stream = ytdl(args[1],{filter:'audioonly'});
 			const dispatcher = connection.play(stream);
 			dispatcher.on('finish', () => message.member.voice.channel.leave());
 		});
-		message.channel.send(args[1]);
+		message.react(ry);
 	}
 	if(message.content===p+'join'){
 		message.member.voice.channel.join();
