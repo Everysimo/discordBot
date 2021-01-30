868c491a487203cdbf14fa183a9697f08415e15f
dany98sa <56975152+dany98sa@users.noreply.github.com>
2021-01-30 15:54:37 +0100

    Update musicBot.js

--- musicBot.js
+++ musicBot.js
@@ -10,34 +10,46 @@ client.once('ready', () => {
 var p=config.prefisso;
 var ry=config.reactionYes;
 var rn=config.reactionNot;
-client.login(process.env.tokenBotDiscord);
 
 client.on('message', message => {
+	//commandi senza argomenti aggiuntivi
 	if (message.content===p+'play') {
 		if (message.member.voice.channel==null) {
 			message.react(rn);
+		}else{
+			message.member.voice.channel.join().then(connection=>{
+				const stream = ytdl('https://www.youtube.com/watch?v=3ekFx8OXxtM',{filter:'videoandaudio'});
+				const dispatcher = connection.play(stream);
+				dispatcher.on('finish', () => message.member.voice.channel.leave());		
+			});
+			message.react(ry);
 		}
-		message.member.voice.channel.join().then(connection=>{
-			const stream = ytdl('https://www.youtube.com/watch?v=3ekFx8OXxtM',{filter:'videoandaudio'});
-			const dispatcher = connection.play(stream);
-			dispatcher.on('finish', () => message.member.voice.channel.leave());		
-		});
-		message.react(ry);
+		return;
 	}
+	if(message.content===p+'join'){
+		if (message.member.voice.channel==null) {
+			message.react(rn);
+		}else{
+			message.member.voice.channel.join();
+			message.react(ry);
+		}
+		return;
+	}
+	//commandi con argomenti aggiuntivi
 	var args=message.content.split(' ');
 	if(args[0]===p+'play'){
 		if (message.member.voice.channel==null) {
 			message.react(rn);
+		}else{
+			message.member.voice.channel.join().then(connection=>{
+				const stream = ytdl(args[1],{filter:'audioonly'});
+				const dispatcher = connection.play(stream);
+				dispatcher.on('finish', () => message.member.voice.channel.leave());
+			});
+			message.react(ry);
 		}
-		message.member.voice.channel.join().then(connection=>{
-			const stream = ytdl(args[1],{filter:'audioonly'});
-			const dispatcher = connection.play(stream);
-			dispatcher.on('finish', () => message.member.voice.channel.leave());
-		});
-		message.react(ry);
-	}
-	if(message.content===p+'join'){
-		message.member.voice.channel.join();
+		return;
 	}
 });
 
+client.login(process.env.tokenBotDiscord);
