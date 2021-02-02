2741d5b58c70497599d04a9135e79b85cd87c7a1
Everysimo <61777854+Everysimo@users.noreply.github.com>
2021-02-02 19:23:44 +0100

    Update musicBot.js

--- musicBot.js
+++ musicBot.js
@@ -354,7 +354,7 @@ client.on("message", message => {
 });
 
 //entrata nuovo utente inserimento dell'utente nel dataBase 
-client.on('guildMemberAdd', member=>{
+/*client.on('guildMemberAdd', member=>{
 	if(!member.user.bot){
 		const nickname=member.user.username;
 		const id=member.user.tag.split("#")[1];
@@ -370,4 +370,4 @@ client.on('guildMemberAdd', member=>{
 			}
 		});
 	}
-});
+});*/
