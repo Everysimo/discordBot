95eda3e6af48644feb2cb483f17ec4a70fe50d8d
Everysimo <61777854+Everysimo@users.noreply.github.com>
2021-02-03 00:48:22 +0100

    Update musicBot.js

--- musicBot.js
+++ musicBot.js
@@ -35,6 +35,7 @@ dbpool.getConnection(function(err){
 		console.log(err);
 		throw new Error("Errore durante la connessione al database");
 	}
+	db.release();
 	console.log("Database connesso!");
 });
 //connessione al database
