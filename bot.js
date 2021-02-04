a0be56b60b7895b319870276cf5b44a3cd244a96
Everysimo <61777854+Everysimo@users.noreply.github.com>
2021-02-04 19:24:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -137,6 +137,8 @@ comandiMusicali.set("setvolume",musica.setvolume);
 
 //mappa comandi non musicali
 let comandi =new Map();
+comandi.set("addsongpl",playlist.addSongToPL);
+comandi.set("rmsongpl",playlist.removeSongToPL);
 comandi.set("slot",gameRoom.slot);
 comandi.set("signin",signIn);
 comandi.set("coinflip",gameRoom.coinflip);
