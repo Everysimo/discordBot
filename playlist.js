efee8a717ccd7c3405ec87e707aa72c5270ccf0e
Everysimo <61777854+Everysimo@users.noreply.github.com>
2021-02-04 18:39:19 +0100

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -4,17 +4,21 @@ const lingua =require(config.lingua);
 const db=require("./dbOpertion.js");
 
 exports.createPlaylist = function (message) {
-    
+    if(!message.member.user.bot){
+    }
 }
 
 exports.addSongToPL = function (message) {
-    
+    if(!message.member.user.bot){
+    }
 }
 
 exports.removeSongFromPL = function (message) {
-    
+    if(!message.member.user.bot){
+    }
 }
 
 exports.printPL = function (message) {
-    
+    if(!message.member.user.bot){
+    }
 }
