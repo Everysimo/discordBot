ec3db4131adc765e01552b48bbf87e9d24a0920e
Everysimo <61777854+Everysimo@users.noreply.github.com>
2021-02-04 12:43:32 +0100

    Update musicBot.js

--- musicBot.js
+++ musicBot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 const ytdl = require('ytdl-core');
-const lingua =require(config.lingua);
+var lingua =require(config.lingua);
 const mysql = require('mysql');
 
 //quando il nuovo cliente è pronto esegue log
