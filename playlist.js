64fb26adf0d1d157c8cc0c98bf9c6b7512812172
Everysimo <61777854+Everysimo@users.noreply.github.com>
2021-02-04 18:42:14 +0100

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -5,20 +5,30 @@ const db=require("./dbOpertion.js");
 
 exports.createPlaylist = function (message) {
     if(!message.member.user.bot){
+        const nomePl=message.content.split(" ")[1];
+    	const id=message.member.user.id;
     }
 }
 
 exports.addSongToPL = function (message) {
     if(!message.member.user.bot){
+        const nomePl=message.content.split(" ")[1];
+        const songUrl=message.content.split(" ")[2];
+        const id=message.member.user.id;
     }
 }
 
 exports.removeSongFromPL = function (message) {
     if(!message.member.user.bot){
+        const nomePl=message.content.split(" ")[1];
+        const songUrl=message.content.split(" ")[2];
+        const id=message.member.user.id;
     }
 }
 
 exports.printPL = function (message) {
     if(!message.member.user.bot){
+        const nomePl=message.content.split(" ")[1];
+    	const id=message.member.user.id;
     }
 }
