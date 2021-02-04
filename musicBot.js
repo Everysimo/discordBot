31ef3d6d84badf378b35e984bfd29c109449257f
Everysimo <61777854+Everysimo@users.noreply.github.com>
2021-02-04 12:58:34 +0100

    Update musicBot.js

--- musicBot.js
+++ musicBot.js
@@ -91,7 +91,7 @@ async function play(message, serverQueue){
 
 		const messaggioAggiuntaCoda = new Discord.MessageEmbed();
 		messaggioAggiuntaCoda.setTitle(lingua.songAddQueue);
-		messaggioAggiuntaCoda.setDescription("["+message.user.name+"]");
+		messaggioAggiuntaCoda.setDescription("[@"+message.user.name+"]");
 		messaggioAggiuntaCoda.addFields({
 		name: song.title,value:" "+song.url}
 		);
@@ -124,7 +124,7 @@ function start(guild, song) {
 
 	const messaggioRiproduzione = new Discord.MessageEmbed();
 	messaggioRiproduzione.setTitle(lingua.startPlay);
-	messaggioRiproduzione.setDescription("["+guild.message.user.name+"]");
+	messaggioRiproduzione.setDescription("[@"+guild.message.user.name+"]");
 	messaggioRiproduzione.addFields({
 		name: song.title,value:" "+song.url}
 		);
