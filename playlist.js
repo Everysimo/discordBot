53e1576b48c380fc6a93de139fd150b508266352
Everysimo <61777854+Everysimo@users.noreply.github.com>
2021-02-04 18:34:52 +0100

    Update playlist.js

---
+++ playlist.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const config = require('./config.json');
+const lingua =require(config.lingua);
+const db=require("./dbOpertion.js");
+
+exports.createPlaylist = function (message) {
+    
+}
+
+exports.addSongToPL = function (message) {
+    
+}
+
+exports.removeSongFromPL = function (message) {
+    
+}
+
+exports.printPL = function (message) {
+    
+}
