1adb16e37b46b38d120a0301019d416093780c24
Everysimo <61777854+Everysimo@users.noreply.github.com>
2021-02-04 20:23:16 +0100

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -7,11 +7,8 @@ exports.createPlaylist = function (message) {
     if(!message.member.user.bot){
         const nomePl=message.content.split(" ")[1];
         const id=message.member.user.id;
-        try {
-            db.createPlayListDB(id, nomePl);
-        } catch (error) {
-            message.reply("playlist gia presente")
-        }
+        
+        db.createPlayListDB(id, nomePl);
     }
 }
 
@@ -20,11 +17,8 @@ exports.addSongToPL = function (message) {
         const nomePl=message.content.split(" ")[1];
         const songUrl=message.content.split(" ")[2];
         const id=message.member.user.id;
-        try {
-            db.addSong(id,songUrl,nomePl)
-        } catch (error) {
-            message.reply("canzone gia presente nella playlist")
-        }
+        
+        db.addSong(id,songUrl,nomePl);
     }
 }
 
