19aba7fd8661fc9d88dc620f926a1dbdea2a7a84
dany98sa <56975152+dany98sa@users.noreply.github.com>
2021-02-05 17:17:40 +0100

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -3,7 +3,6 @@ const config = require('./config.json');
 const lingua =require(config.lingua);
 const db=require("./dbOpertion.js");
 const ytdl = require('ytdl-core');
-const musica=require("./musica.js")
 
 exports.createPlaylist = function (message) {
     if(!message.member.user.bot){
@@ -63,5 +62,70 @@ exports.playPL= function (message, serverQueue) {
     const nomePl=message.content.split(" ")[1];
     const id=message.member.user.id;
     db.leggiPL(id, nomePl,async function(risult){
+        if (risult) {
+            play(message,risult[0].song,serverQueue);
+        }
     });
+}
+
+play= async function (message, songUrl,serverQueue){
+	const voiceChannel = message.member.voice.channel;	//connessione al canale vocale
+  	if (!voiceChannel){									//se l'utente non è in un canale genera eccezione
+		return message.reply(lingua.voiceChannelNotFound);
+	}
+
+	const permissions = voiceChannel.permissionsFor(message.client.user);	//verifica permessi utente che richiama il messggio
+  	if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+    	return message.reply(lingua.voiceChannelNotPermission);
+	}
+	var songInfo;
+
+	try{
+		songInfo = await ytdl.getInfo(songUrl);			//ottiene informazioni della canzone passata come argomento
+	}
+	catch(err){
+		throw new Error("errore nel caricamento dell informazioni della canzone");
+	}
+	
+	var song = {
+    	title: songInfo.videoDetails.title,
+		url: songInfo.videoDetails.video_url,
+		isLive: songInfo.videoDetails.isLiveContent,
+		username: message.member.user.username,
+	};
+
+	if (!serverQueue) {					//se la coda delle canzoni è vuota
+		const queueContruct = {
+			textChannel: message.channel,
+			voiceChannel: voiceChannel,
+			connection: null,
+			songs: [],
+			volume: 50,
+			playing: true,
+		};
+		queue.set(message.guild.id, queueContruct);
+		queueContruct.songs.push(song);
+		try {
+			var connection = await voiceChannel.join();	//connessione al canale vocale dell'utente che invia il messaggio
+			queueContruct.connection = connection;			
+			this.start(message.guild, queueContruct.songs[0]);	//starata la prima canzone in coda
+		} catch (err) {
+			console.log(err.stack);
+			queue.delete(message.guild.id);
+			return message.reply(lingua.errorJoinVoiceChannel);
+		}
+	}
+	else{	//se la coda delle canzoni non è vuota aggiunge la canzone alla coda
+
+		serverQueue.songs.push(song);
+
+		const messaggioAggiuntaCoda = new Discord.MessageEmbed();
+		messaggioAggiuntaCoda.setTitle(lingua.songAddQueue);
+		messaggioAggiuntaCoda.setDescription("[ @"+message.member.user.username+" ]");
+		messaggioAggiuntaCoda.addFields({
+		name: song.title,value:" "+song.url}
+		);
+		return message.reply(messaggioAggiuntaCoda);
+		//return message.reply(song.title +" "+ lingua.songAddQueue)
+	}
 }
