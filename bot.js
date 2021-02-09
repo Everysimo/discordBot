3e80a38092ec1c104941d67d6232cf93bb87ba90
dany98sa <56975152+dany98sa@users.noreply.github.com>
2021-02-09 20:22:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,8 +22,9 @@ client.once('ready', () => {
 const p=config.prefixCommand;
 
 
-var countUser=client.guilds.cache.get("341919077008146432").memberCount;
-client.guilds.cache.get("341919077008146432").channels.cache.get("808772063446827068").setName("online-user:"+countUser);
+var guild=client.guilds.cache.get("341919077008146432");
+var countUser=guild.members.cache.size;
+guild.cache.get("808772063446827068").setName("online-user:"+countUser);
 
 //login nel server tramite token
 client.login(process.env.tokenBotDiscord);
