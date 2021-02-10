c0b9a174544f456e27ff279c1977709a9a2e6ddf
dany98sa <56975152+dany98sa@users.noreply.github.com>
2021-02-10 19:04:44 +0100

    add random.js

---
+++ random.js
@@ -0,0 +1,7 @@
+/*const KSoftClient = require('@ksoft/api');
+const ksoft = new KSoftClient(process.env.tokenKsoft);
+
+exports.image= async function (message) {
+    const url = await ksoft.images.aww();
+    message.channel.send(url);
+}*/
