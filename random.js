1e8e50935d34c7f04886a61fb011c4142ea833c9
dany98sa <56975152+dany98sa@users.noreply.github.com>
2021-02-10 19:14:34 +0100

    Update random.js

--- random.js
+++ random.js
@@ -1,8 +1,8 @@
-const KSoftClient = require('@ksoft/api');
+const {KSoftClient} = require('@ksoft/api');
 const ksoft = new KSoftClient(process.env.tokenKsoft);
 
 exports.image= async function (message) {
     const tag=message.message.content.split(" ")[1]
-    const url = await ksoft.images.random(tag, { nsfw: false });
+    const {url} = await ksoft.images.random(tag, { nsfw: false });
     message.channel.send(url);
 }
