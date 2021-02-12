c672d2dce93ed49759b8ee6d87420b428b61975a
Everysimo <61777854+Everysimo@users.noreply.github.com>
2021-02-12 23:31:43 +0100

    Update user.js

--- user.js
+++ user.js
@@ -32,7 +32,7 @@ exports.addTime = addTime;
 function applyAddTime(id){
 	getTempoOnlineSeconds(id,function(tempoOnline,daysOnline){
 		
-		tempoOnline+=5;
+		tempoOnline+=1;
 		if(tempoOnline>=86400){
 			daysOnline++;
 			tempoOnline-=86400;
