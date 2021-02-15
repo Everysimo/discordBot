7f999c7c57c10d7042e93fd111a6f0708bb2eb77
dany98sa <56975152+dany98sa@users.noreply.github.com>
2021-02-15 18:59:42 +0100

    Update user.js

--- user.js
+++ user.js
@@ -20,12 +20,16 @@ function applyAddCoin(id){
 }
 
 async function addTime(){ 
-	const guild = bot.client.guilds.cache.get(config.IdServer);
-	const activeMember= await guild.members.cache.filter(member=>member.voice.channel!==null).array();
-	for (let index = 0; index < activeMember.length; index++) {
-		var id = activeMember[index].id;
-		applyAddTime(id)
+	const guild = bot.client.guilds.cache.array();
+	for (let i = 0; i < guild.length; i++) {
+		const element = guild[i];
+		const activeMember= await element.members.cache.filter(member=>member.voice.channel!==null).array();
+		for (let index = 0; index < activeMember.length; index++) {
+			var id = activeMember[index].id;
+			applyAddTime(id)
+		}
 	}
+	
 }
 exports.addTime = addTime;
 
