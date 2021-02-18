64c07eb7e3e86d40e608b058391c8798f9533e1b
Everysimo <61777854+Everysimo@users.noreply.github.com>
2021-02-18 19:10:07 +0100

    Update user.js

--- user.js
+++ user.js
@@ -64,8 +64,33 @@ exports.aggiornaSaldo = aggiornaSaldo;
 function aggiornaRuolo(user,days){
 	switch(days){
 		case 7:
-			user.roles.remove("812012075114561536","almeno 7 giorni online");
-			user.roles.add("812013606308675616","7 giorni online");
+			try{
+				user.roles.remove("812012075114561536","almeno 7 giorni online");
+			}
+			catch(err){
+				console.log("ruolo not found");
+			}
+			try {
+				user.roles.add("812013606308675616","7 giorni online");
+			}
+			catch(err){
+				console.log("errore nell'aggingere il nuovo ruolo");
+			}
+			break;
+		case 28:
+			try{
+				user.roles.remove("812013606308675616","almeno 28 giorni online");
+			}
+			catch(err){
+				console.log("ruolo not found");
+			}
+			try {
+				user.roles.add("812022805926379550","28 giorni online");
+			}
+			catch(err){
+				console.log("errore nell'aggingere il nuovo ruolo");
+			}
+			break
 	}
 }
 
