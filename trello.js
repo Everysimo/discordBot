5c7268760de0c5363783e3656523052250e83703
dany98sa <56975152+dany98sa@users.noreply.github.com>
2021-02-20 03:11:04 +0100

    Update trello.js

--- trello.js
+++ trello.js
@@ -3,8 +3,7 @@ var trello=new Trello(process.env.trelloKey,process.env.trelloToken);
 
 function addSuggestion(message) {
     var titolo="by: "+message.member.user.id;
-    var descrizione=message.content.split(" ");
-    descrizione.shift();
+    var descrizione=message.content.substr(message.content.indexOf(" "));
     trello.addCard(titolo,descrizione,"602fffe7f6c57a0f8f77bc02",error=>{
         if (error) {
             console.log(error);
