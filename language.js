941b8cd92cb824baf2e001eb7f42f737f333011c
dany98sa <56975152+dany98sa@users.noreply.github.com>
2021-02-24 19:52:04 +0100

    Update language.js

--- language.js
+++ language.js
@@ -1,11 +1,29 @@
 const langPackage = require("langpackage");
+const fs = require("fs");
+
 var langPack ={};
 exports.langPack = langPack;
-langPack.en=new langPackage();
-langPack.it=new langPackage();
 
-langPack.en.pushPhrase('hello', 'hello word');
-langPack.it.pushPhrase('hello', 'ciao mondo');
-
-console.log(langPack['it'].get('hello'));
-console.log(langPack['en'].get('hello'));
+fs.readdir("./language",(err, data) => {
+    if (err) {
+        throw err;
+    }
+    for (let index = 0; index < data.length; index++) {
+        const element = data[index];
+        fs.readdir("./language/"+element,(err1, data1) => {
+            if (err1) {
+                throw err1;
+            }
+            for (let index = 0; index < data1.length; index++) {
+                const element1 = data1[index];
+                fs.readFile("./language/"+element+"/"+element1,(err2, data2) => {
+                    if (err2) {
+                        throw err2;
+                    }
+                    langPack[element]=new langPackage();
+                    langPack[element].importJSON(data2.toString())
+                });
+            }
+        });
+    }
+})
