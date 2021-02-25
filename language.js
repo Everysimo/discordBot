7f19a4abdc22b0358c51850378924135b88aabbe
dany98sa <56975152+dany98sa@users.noreply.github.com>
2021-02-25 18:33:59 +0100

    Update language.js

--- language.js
+++ language.js
@@ -14,16 +14,15 @@ fs.readdir("./language",(err, data) => {
             if (err1) {
                 throw err1;
             }
+            langPack[element]=new langPackage();
             for (let index = 0; index < data1.length; index++) {
                 const element1 = data1[index];
                 fs.readFile("./language/"+element+"/"+element1,(err2, data2) => {
                     if (err2) {
                         throw err2;
                     }
-                    langPack[element]=new langPackage();
                     langPack[element].importJSON(data2.toString())
                 });
-                break;
             }
         });
     }
