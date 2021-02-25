9c4d7006a2987919344b2586f237632e5c3764a3
dany98sa <56975152+dany98sa@users.noreply.github.com>
2021-02-25 19:08:43 +0100

    migliorie

--- language.js
+++ language.js
@@ -3,22 +3,19 @@ const fs = require("fs");
 
 var langPack ={};
 exports.langPack = langPack;
-
 fs.readdir("./language",(err, data) => {
     if (err) {
         throw err;
     }
     for (let index = 0; index < data.length; index++) {
         var element = data[index];
-        fs.readdir("./language/"+element,async (err1, data1) => {
+        fs.readdir("./language/"+element,(err1, data1) => {
             if (err1) {
                 throw err1;
             }
             langPack[element]=new langPackage();
-            for (let index = 0; index < data1.length; index++) {
-                var element1 = data1[index];
-                await langPack[element].importJSON(fs.readFileSync("./language/"+element+"/"+element1).toString());
-            }
+            langPack[element].importJSON(fs.readFileSync("./language/"+element+"/"+data1[0]).toString());
+            console.log(langPack)
         });
     }
 })
